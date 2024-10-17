1f3f107bd5b2a1decde6af9ea8dc76a04fd8e6e7
PC <clarin58588@gmail.com>
2024-10-16 20:02:35 -0500

    Adding service interface

--- src/main/java/culturemedia/model/Video.java
+++ src/main/java/culturemedia/model/Video.java
@@ -1,5 +1,28 @@
 package culturemedia.model;
 
-public record Video(String code, String title, String description, Double duration) {
+public class Video {
 
+    private int id;          // Atributo privado para el ID
+    private String title;
+    private String description;
+
+    // Constructor
+    public Video(int id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    // Método getter para obtener el ID
+    public int getId() {
+        return id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
 }

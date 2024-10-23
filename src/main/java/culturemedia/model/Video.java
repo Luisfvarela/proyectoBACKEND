04bc0f6bcd051966a495b981aca078e13fbc3aee
Luisfvarela <clarin58588@gmail.com>
2024-10-23 08:55:00 -0500

    Update Video.java

--- src/main/java/culturemedia/model/Video.java
+++ src/main/java/culturemedia/model/Video.java
@@ -1,28 +1,4 @@
 package culturemedia.model;
 
-public class Video {
-
-    private int id;          // Atributo privado para el ID
-    private String title;
-    private String description;
-
-    // Constructor
-    public Video(int id, String title, String description) {
-        this.id = id;
-        this.title = title;
-        this.description = description;
-    }
-
-    // Método getter para obtener el ID
-    public int getId() {
-        return id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public String getDescription() {
-        return description;
-    }
+public record Video(String code, String title, String description, Double duration) {
 }
